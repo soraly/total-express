a4c8068bd0768b15550b4c70e71e72dd7ab5485a
刘志翔 <soraly@163.com>
2018-03-29 15:50:12 +0800

    newbranch commit

--- index.js
+++ index.js
@@ -13,7 +13,8 @@ const mysql = require('mysql')
 const urlLib = require('url')
 var server = express();
 var upload = multer({dest: './template/uploadedImg'})
-console.log(12345)
+alert(123)
+console.log(1234567)
 //测试NODE_ENV
 console.log(process.env.NODE_ENV,'process.env.NODE_ENV')
 if(process.env.NODE_ENV == 'develop'){
