3233ea59bfb68c39a1ac66b521db7ba0ca330add
刘志翔 <soraly@163.com>
2018-03-29 15:56:18 +0800

    new11

--- index.js
+++ index.js
@@ -13,7 +13,6 @@ const mysql = require('mysql')
 const urlLib = require('url')
 var server = express();
 var upload = multer({dest: './template/uploadedImg'})
-alert(123)
 console.log(1234567)
 //测试NODE_ENV
 console.log(process.env.NODE_ENV,'process.env.NODE_ENV')
